b53a99932e2f6aabffebfd030f973b7dde62bfea
yashtyagi13 <yashtyaiiitl332@gmail.com>
2025-04-04 12:17:53 +0530

    modify

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,15 @@ import Footer from "../../components/Footer";
 import React from "react";
 
 export default function About() {
+
+
   return (
     <>
       <Header />
       <main style={{ padding: "2rem" }}>
         <h1>About Us</h1>
         <p>
-          This page is about Oranzon and our journey in crafting digital
-          experiences.
+          This page is about Oranzon and our journey in crafting digital experiences.
         </p>
       </main>
       <Footer />
